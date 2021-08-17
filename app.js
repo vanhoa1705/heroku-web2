459c9f576ac933b8736f7e7bf230b8453bd98e45
Nguyễn Văn Hoà <1712448@student.hcmus.edu.vn>
2021-08-17 15:50:17 +0700

    fix

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use("*", (req, res, next) => {
 
 app.use(handlerError);
 
-const PORT = 5000;
-app.listen(process.env.PORT | PORT, function () {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
   console.log(`Back-end api is running at http://localhost:${PORT}`);
 });
